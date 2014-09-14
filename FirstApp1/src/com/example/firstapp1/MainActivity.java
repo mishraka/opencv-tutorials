2f42451cbf2dbbcc6cbc2c80f37d8a0eaad9cd78
PrarthanaMishra <theprarthana@gmail.com>
2014-09-14 21:42:55 +0530

    k

---
+++ FirstApp1/src/com/example/firstapp1/MainActivity.java
@@ -0,0 +1,17 @@
+
+package com.example.firstapp1;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	}
+
+	
